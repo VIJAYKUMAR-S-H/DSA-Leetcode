b6d2107bed2823bd323d8a402addeebd4ec13d4c
Vijaykumar s h <vijaykumarhadimani77@gmail.com>
2026-03-16 21:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (15.58%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.c
@@ -0,0 +1,20 @@
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+
+    struct ListNode* curr = head;
+
+    while(curr != NULL && curr->next != NULL)
+    {
+        if(curr->val == curr->next->val)
+        {
+            struct ListNode* temp = curr->next;
+            curr->next = curr->next->next;
+            free(temp);
+        }
+        else
+        {
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
