ff68ae52213919717845d7ad5635e74268c5ff4a
Vijaykumar s h <vijaykumarhadimani77@gmail.com>
2026-03-16 21:27:54 +0530

    Time: 7 ms (31.97%) | Memory: 9.7 MB (9.65%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+int compare(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+int minimumDifference(int* nums, int numsSize, int k) {
+
+    if(k == 1)
+        return 0;
+
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int minDiff = 1000000;
+
+    for(int i = 0; i <= numsSize - k; i++)
+    {
+        int diff = nums[i + k - 1] - nums[i];
+
+        if(diff < minDiff)
+            minDiff = diff;
+    }
+
+    return minDiff;
+}
