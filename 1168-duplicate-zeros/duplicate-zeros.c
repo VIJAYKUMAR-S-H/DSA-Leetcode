d16bb01720427b04d3a8dae7ecf1b7271f2bfc12
Vijaykumar s h <vijaykumarhadimani77@gmail.com>
2026-03-30 20:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (37.50%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.c
+++ 1168-duplicate-zeros/duplicate-zeros.c
@@ -1,11 +1,32 @@
-void duplicateZeros(int* arr, int arrSize) {
-    for(int i = 0; i < arrSize; i++) {
+void duplicateZeros(int* arr, int n) {
+    int zeros = 0;
+
+    // Count zeros
+    for(int i = 0; i < n; i++) {
         if(arr[i] == 0) {
-            // shift elements to right
-            for(int j = arrSize - 1; j > i; j--) {
-                arr[j] = arr[j - 1];
-            }
-            i++; // skip next element (new zero)
+            zeros++;
         }
+    }
+
+    int i = n - 1;
+    int j = n + zeros - 1;
+
+    // Traverse from back
+    while(i >= 0) {
+        // Copy element if within bounds
+        if(j < n) {
+            arr[j] = arr[i];
+        }
+
+        // If zero, duplicate it
+        if(arr[i] == 0) {
+            j--;
+            if(j < n) {
+                arr[j] = 0;
+            }
+        }
+
+        i--;
+        j--;
     }
 }
