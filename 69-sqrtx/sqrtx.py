d56fc900f5fd90e7d2ffd14738d0cd004f6ac323
Vijaykumar s h <vijaykumarhadimani77@gmail.com>
2026-04-09 21:53:24 +0530

    Time: 4 ms (43.37%) | Memory: 12.3 MB (92.88%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mySqrt(self, x):
+        low, high = 0, x
+        ans = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return ans
