fac9abb06e2065729ce261e124eacaad3f5ed561
Vijaykumar s h <vijaykumarhadimani77@gmail.com>
2026-04-16 20:40:36 +0530

    Time: 19 ms (80.21%) | Memory: 13.2 MB (27.02%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,10 +5,14 @@ class Solution(object):
         max_length = 0
 
         for right in range(len(s)):
+            # If character already seen, move left pointer
             if s[right] in char_map:
                 left = max(left, char_map[s[right]] + 1)
 
+            # Update latest index of character
             char_map[s[right]] = right
+
+            # Update maximum length
             max_length = max(max_length, right - left + 1)
 
         return max_length
