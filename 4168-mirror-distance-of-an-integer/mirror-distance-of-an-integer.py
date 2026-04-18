c7b28424fbb128a65a113aac167ebae916ee24f4
Vijaykumar s h <vijaykumarhadimani77@gmail.com>
2026-04-18 23:55:29 +0530

    Time: 2 ms (23.51%) | Memory: 12.1 MB (100.00%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.py
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.py
@@ -1,10 +1,3 @@
 class Solution(object):
     def mirrorDistance(self, n):
-        rev = 0
-        temp = n
-        
-        while temp > 0:
-            rev = rev * 10 + temp % 10
-            temp //= 10
-        
-        return abs(n - rev)
+        return abs(n - int(str(n)[::-1]))
